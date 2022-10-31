147d91ec66c01dfe10fd648d358bcafec12c4727
andrea gauvreau <andrea.gauvreau31@gmail.com>
2022-10-31 12:05:06 +0100

    app ready to work

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
 <div className='app'>
 <SearchBar />
-<GifContainer />
 </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,11 +9,13 @@ import './GifContainer.css'
 export default function SearchBar(){
     const apiKey="gsBk1DGLQzrS8aUuMmV85C1whQgHu3kr"
     const [data, setData]=useState([])
-    const [search, setSearch]=useState('teachizi')
+    const [search, setSearch]=useState('')
     const searchitem = useRef()
+    const [number, setNumber]= useState('1')
+    const searchNumber = useRef()
 
     useEffect(()=>{
-        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}`)
+        fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=${number}`)
       .then(res => res.json())
       .then(res => setData(res.data))
       })
@@ -26,20 +28,31 @@ export default function SearchBar(){
         e.preventDefault()
         setSearch(searchitem.current.value)
       }
-      
+      const handleNumberChange=(e)=>{
+        setNumber(searchNumber.current.value)
+      }
 
-    return(<>
-
-    <h2>Cherche ton Gif Préféré</h2>
-
-    <input type='text'ref={searchitem}/>
-
-    <input type='submit' onClick={handleSubmit}/>
-    <div id="container_of_gif">
-    {
-    data.map(e=> (<GifContainer dataImg = {e.images.downsized.url} dataText={e.id}/>))
-  }
-    </div>
+    return(
+    <>
+        <h2>Cherche ton Gif Préféré</h2>
+        <div id="searchBar">
+            <input id='search' type='text'ref={searchitem} placeholder='Search...'/>
+            <input id='submit'type='submit' onClick={handleSubmit}/>
+        </div>
+        <label>
+            Combien de Gif afficher ?
+            <select id='number' ref={searchNumber} onChange={handleNumberChange}>
+                    <option value='1'>1</option>
+                    <option value='5'>5</option>
+                    <option value='10'>10</option>
+                    <option value='20'>20</option>
+                    <option value='50'>50</option>
+                    <option value='200'>200</option>
+            </select>
+        </label>
+        <div id="container_of_gif">
+            {data.map(e=> (<GifContainer dataImg = {e.images.downsized.url} dataText={e.id}/>))}
+        </div>
 
     </>)
 }
